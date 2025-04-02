d5e94f82c528cee8ea2736603913b9847df85b71
Maheshwari1912 <mahiratthapuram@gmail.com>
2025-04-02 08:09:17 +0530

    Update booth multiplier.v

--- booth multiplier.v
+++ booth multiplier.v
@@ -1,27 +1,27 @@
 module booth_multiplier_4bit (
     input signed [3:0] A, B,
-    output reg signed [7:0] P
+    output reg signed [7:0] product
 );
     reg [7:0] Q, M, N;
     reg Q_1;
     reg [3:0] count;
 
     always @(A or B) begin
-        A=8'd0;
+        N=8'd0;
         M={A, 4'd0};
         Q = {4'd0, B};
         Q_1=1'b0;
         count= 4'd4;
         while(count>0) begin
             case({Q[0],Q_1})
-                2'b01: A=A+M;
-                2'b10: A=A-M;
+                2'b01: N=N+M;
+                2'b10: N=N-M;
             endcase
             Q_1 = Q[0];
-            Q = {A[0], Q[7:1]};
-            A = {A[7], A[7:1]};
+            Q = {N[0], Q[7:1]};
+            N = {N[7], N[7:1]};
             count = count-1;
         end
-        product = {A[7:0]};           
+        product = {N[7:0]};           
     end
 endmodule
